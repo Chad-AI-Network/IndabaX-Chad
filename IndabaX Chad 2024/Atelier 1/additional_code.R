bff58a054d7bfe49f72decb3313d4d2280a0b227
Abdel-aziz Harane <122400488+abdelazizharane@users.noreply.github.com>
2024-10-27 07:12:31 +0100

    Supports

---
+++ IndabaX Chad 2024/Atelier 1/additional_code.R
@@ -0,0 +1,5 @@
+# Include bash code in Rstudio
+#!/bin/bash
+# for ((i=1; i<=10; i++)); do
+# echo $i
+# done
---
+++ IndabaX Chad 2024/Atelier 1/mypackages.R
@@ -0,0 +1,3 @@
+pkgs <- c("tidyverse", "lubridate", "tidyr", "stringr", "stringi", "kableExtra",
+          "reshape2")
+p_load(pkgs, character.only = TRUE)
